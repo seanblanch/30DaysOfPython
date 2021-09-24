fae409000cbc0d137df3b1a7f6be992de63918d2
sean199251 <sean199251@hotmail.com>
2021-09-24 13:14:16 -0700

    Update Variables.py

--- day_2/Variables.py
+++ day_2/Variables.py
@@ -17,4 +17,31 @@ is_light_on = False
 multi_variable_tuple = (1, 2, 3, 4, 5, 6, 7) #tuple
 mult_variable_list = [1, 2, 'Sean', 6, 'college'] #list
 
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+print(multi_variable_tuple)
+print(mult_variable_list)
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(multi_variable_tuple))
+print(type(mult_variable_list))
+
+print('The length of my first name is: ', len(first_name))
+
+
 
