e3cb1222f60883a3f7e696f141c2df1fe637192b
sean199251 <sean199251@hotmail.com>
2021-09-30 19:12:29 -0700

    finished day 4

--- day_4/ExercisesDay4.py
+++ day_4/ExercisesDay4.py
@@ -74,7 +74,86 @@ print(company[-1])
 print(company[10]) #space
 
 # Create an acronym or an abbreviation for the name 'Python For Everyone'.
-if any(c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for c in company):
-    print(company[0])
+p_f_e = 'Python For Everyone'
+res = [char for char in p_f_e if char.isupper()]
+print(''.join(res))
 
+# Create an acronym or an abbreviation for the name 'Coding For All'.
+res2 = [char for char in company if char.isupper()]
+print(''.join(res2))
 
+# Use index to determine the position of the first occurrence of C in Coding For All.
+print(company.index('C'))
+
+# Use index to determine the position of the first occurrence of F in Coding For All.
+print(company.index('F'))
+
+# Use rfind to determine the position of the last occurrence of l in Coding For All People.
+print(company.rfind('l'))
+
+# Use index or find to find the position of the first occurrence of the word 'because' in the following sentence:
+# 'You cannot end a sentence with because because because is a conjunction'
+new_sentence = 'You cannot end a sentence with because because because is a conjunction'
+print(new_sentence.index('because'))
+
+# Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot
+# end a sentence with because because because is a conjunction'
+print(new_sentence.rindex('because'))
+
+# Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because
+# because because is a conjunction'
+print(new_sentence[31:54])
+
+# Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a
+# sentence with because because because is a conjunction'
+second_sentence = 'You cannot end a sentence with because because because is a conjunction'
+print(second_sentence.index('because'))
+
+# Does ''Coding For All' start with a substring Coding?
+if company.find('Coding') == 0:
+    print('it starts with Coding')
+else:
+    print('it does not start with Coding')
+
+# Does 'Coding For All' end with a substring coding?
+if company.endswith('coding'):
+    print('The string ends with coding')
+else:
+    print('The string does not end with Coding')
+
+# '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+c_f_a_string = '\tCoding For All\t\t'
+print(c_f_a_string.expandtabs(-1))
+
+# Which one of the following variables return True when we use the method isidentifier():
+# 30DaysOfPython
+# thirty_days_of_python
+challenge = '30DaysOfPython'
+print(challenge.isidentifier())
+challenge = 'thirty_days_of_python'
+print(challenge.isidentifier())
+
+# The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid',
+# 'Falcon']. Join the list with a hash with space string.
+lists = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('# '.join(lists))
+
+# Use the new line escape sequence to separate the following sentences.
+# I am enjoying this challenge.
+# I just wonder what is next.
+sentence_one = 'I am enjoying this challenge.'
+sentence_two = 'I just wonder what is next.'
+sentence_combined = sentence_one + '\n' + sentence_two
+print(sentence_combined)
+
+# Use a tab escape sequence to write the following lines.
+tab_sentence = 'Name\tAge \tCountry \tCity \nAsabeneh \t250 \tFinland \tHelsinki'
+print(tab_sentence)
+
+# Use the string formatting method to display the following:
+string_formatting = 'radius = 10 \narea = 3.14 * radius ** 2 \nThe area of a circle with raidus 10 is 314 meters square.'
+print(string_formatting)
+
+# Make the following using string formatting methods:
+string_formatting = '8 + 6 = 14 \n8 - 6 = 2 \n8 * 6 = 48 \n8 / 6 = 1.33 \n8 % 6 = 2 \n8 // 6 = 1 \n8 ** 6 = 262144'
+print(string_formatting)
